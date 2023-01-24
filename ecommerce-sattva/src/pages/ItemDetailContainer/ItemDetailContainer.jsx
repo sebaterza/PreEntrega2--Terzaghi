7b7b74a26c8383c2b4f63ac5711bd548d543a57d
Sebastian Terzaghi <sebaterza@gmail.com>
2023-01-24 16:17:54 -0300

    ultimo commit, segunda pre entrega actualizada

--- ecommerce-sattva/src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ ecommerce-sattva/src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react"
 import { getProducts } from "../../Mock/Productos";
 import ItemDetail from "../../componentes/ItemDetail/ItemDetail"
-import { useFetcher, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getProduct } from "../../Mock/Productos";
 
 const ItemDetailContainer = () => {
   const [detalleProducto, setDetalleProducto] = useState({});
   const { id } = useParams();
   useEffect(() => {
-    getProduct()
-      .then((res) => setDetalleProducto(res.find((item) => item.id === id)))
+    getProduct(id)
+      .then( (res => {setDetalleProducto(res)}))
       .catch((error) => console.log(error))
   }, [id]);
 
